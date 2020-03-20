eccbea0b78c0992f9f2c1635274838b62c745499
Lucas Gregory <lgregory12@hotmail.com>
2020-03-19 21:33:28 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <v-container id="nav">
+      <router-link to="/">Dashboard</router-link> |
+      <router-link to="/users">Usuários</router-link>
+    </v-container>
+    <v-app>
+      <router-view />
+    </v-app>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/network/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+  // Servidor de mock local.
+  // Rode `yarn mock` para subir o servidor
+  baseURL: "http://localhost:3000"
+});
+
+export default api;
