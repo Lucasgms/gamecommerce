af87d7405f93e7d03ed3c1edfd9cd91939ddb82c
Lucas Gregory <lgregory12@hotmail.com>
2020-03-24 11:29:12 -0300

    Add game card component.

---
+++ src/modules/games/components/GameCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-hover>
+    <template v-slot="{ hover }">
+      <v-card id="game-card" outlined>
+        <div class="card-header pa-4 mb-2 grey lighten-2">
+          <v-img
+            class="align-self-center mx-auto my-6"
+            :src="require(`@/assets/${game.image}`)"
+            height="180px"
+            aspect-ratio="1.7"
+            contain
+          />
+        </div>
+        <div class="card-description" v-show="!hover">
+          <v-card-subtitle class="subtitle-1 text-center pa-2">
+            {{ game.name }}
+          </v-card-subtitle>
+          <v-card-subtitle
+            class="subtitle-2 text-center pa-0 mb-3 blue--text text--lighten-1"
+          >
+            {{ game.price | number("$ 0.0,") }}
+          </v-card-subtitle>
+        </div>
+        <v-card-actions class="actions pa-3 justify-center" v-show="hover">
+          <v-btn color="blue" elevation="0" text large>
+            Adicionar ao carrinho
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-hover>
+</template>
+<script>
+export default {
+  name: "GameCard",
+  props: {
+    game: Object
+  }
+};
+</script>
+<style>
+#game-card {
+  border: none;
+  cursor: pointer;
+}
+
+#game-card .card-description,
+#game-card .actions {
+  height: 75px;
+}
+</style>
--- src/modules/games/pages/Index.vue
+++ src/modules/games/pages/Index.vue
@@ -15,48 +15,20 @@
     </v-row>
     <v-row>
       <v-col v-for="item in list" :key="item.id" cols="12" sm="4">
-        <v-hover>
-          <template v-slot="{ hover }">
-            <v-card id="game-card" outlined>
-              <div class="card-header pa-4 mb-2 grey lighten-2">
-                <v-img
-                  class="align-self-center mx-auto my-6"
-                  :src="require(`@/assets/${item.image}`)"
-                  height="180px"
-                  aspect-ratio="1.7"
-                  contain
-                />
-              </div>
-              <div class="card-description" v-show="!hover">
-                <v-card-subtitle class="subtitle-1 text-center pa-2">
-                  {{ item.name }}
-                </v-card-subtitle>
-                <v-card-subtitle
-                  class="subtitle-2 text-center pa-0 mb-3 blue--text text--lighten-1"
-                >
-                  {{ item.price | number("$ 0.0,") }}
-                </v-card-subtitle>
-              </div>
-              <v-card-actions
-                class="actions pa-3 justify-center"
-                v-show="hover"
-              >
-                <v-btn color="blue" elevation="0" text large>
-                  Adicionar ao carrinho
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-hover>
+        <GameCard :game="item" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
 import { mapState, mapGetters, mapActions, mapMutations } from "vuex";
+import GameCard from "../components/GameCard.vue";
 
 export default {
   name: "GamesIndex",
+  components: {
+    GameCard
+  },
   mounted() {
     this.getList();
   },
@@ -81,15 +53,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#game-card {
-  border: none;
-  cursor: pointer;
-}
-
-#game-card .card-description,
-#game-card .actions {
-  height: 75px;
-}
-</style>
