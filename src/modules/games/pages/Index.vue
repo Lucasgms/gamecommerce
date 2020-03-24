b4ab22c7775c334c2e24139d655c38a8246723eb
Lucas Gregory <lgregory12@hotmail.com>
2020-03-24 12:09:36 -0300

    Cart empty state.

--- src/modules/games/pages/Index.vue
+++ src/modules/games/pages/Index.vue
@@ -1,21 +1,45 @@
 <template>
   <v-container>
-    <v-row class="mb-8">
-      <v-col cols="12" sm="6">
-        <h1>Games</h1>
-      </v-col>
-      <v-col cols="12" sm="3" offset-sm="3">
-        <v-select
-          :items="ordenationOptions"
-          :full-width="false"
-          v-model="listOrder"
-          outlined
-        />
-      </v-col>
-    </v-row>
     <v-row>
-      <v-col v-for="item in list" :key="item.id" cols="12" sm="4">
-        <GameCard :game="item" />
+      <v-col cols="12" sm="8">
+        <v-container>
+          <v-row class="mb-8">
+            <v-col cols="12" sm="6">
+              <h1>Games</h1>
+            </v-col>
+            <v-col cols="12" sm="4" offset-sm="2">
+              <v-select
+                :items="ordenationOptions"
+                :full-width="false"
+                v-model="listOrder"
+                outlined
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col v-for="item in list" :key="item.id" cols="12" sm="4">
+              <GameCard :game="item" />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+      <v-col class="pa-9" cols="12" sm="4">
+        <v-card outlined>
+          <v-card-title class="font-weight-bold">
+            Carrinho
+          </v-card-title>
+          <div class="card-content--empty my-8">
+            <v-img
+              class="align-self-center mx-auto"
+              src="@/assets/cart-icon.svg"
+              width="80px"
+            />
+            <v-card-subtitle class="text-center">
+              Até o momento,<br />
+              o seu carrinho está vazio
+            </v-card-subtitle>
+          </div>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
